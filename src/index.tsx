1cabff26f9cf489172e6e432bd95d17a1e89300d
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-07-17 19:29:23 +0300

    added element

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+const title = React.createElement("h1", { id: "title" }, "Hello React");
+
+//
+const h1 = <h1 id="title-2">Hello React.js</h1>;
+const content = (
+  <div>
+    <p className="text">Some React.js</p>
+    <p className="text">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+      pariatur unde. Assumenda dolorem iusto modi rem amet debitis laborum
+      tenetur quae, quibusdam reprehenderit dolores! Ducimus est molestiae harum
+      incidunt quas!
+    </p>
+    <p className="text">Some React.js</p>
+  </div>
 );
-root.render(<React.StrictMode></React.StrictMode>);
+
+// function App() {
+//   return (
+//     <div>
+//       <p className="text">Some React.js</p>
+//       <p className="text">
+//         Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+//         pariatur unde. Assumenda dolorem iusto modi rem amet debitis laborum
+//         tenetur quae, quibusdam reprehenderit dolores! Ducimus est molestiae
+//         harum incidunt quas!
+//       </p>
+//     </div>
+//   );
+// }
+const App = () => {
+  return (
+    <div>
+      <p className="text">Some React.js</p>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis,
+        pariatur unde. Assumenda dolorem iusto modi rem amet debitis laborum
+        tenetur quae, quibusdam reprehenderit dolores! Ducimus est molestiae
+        harum incidunt quas!
+      </p>
+    </div>
+  );
+};
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    {/* {h1},{title},{content} */} <App />
+  </React.StrictMode>
+);
