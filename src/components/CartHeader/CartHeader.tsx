c123403ab779981b2ddef7453dcce8469c3111b8
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-08-04 19:56:32 +0300

    added total amount

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -13,12 +13,24 @@ const CartHeader = ({ productsInCart }: Props) => {
 
   return (
     <div>
-      {Object.keys(productsInCart).map((productId) => (
-        <div key={productId}>
-          {productsObject[parseInt(productId)].title}:{" "}
-          {productsInCart[parseInt(productId)]}
-        </div>
-      ))}
+      <div>
+        {Object.keys(productsInCart).map((productId) => (
+          <div key={productId}>
+            {productsObject[parseInt(productId)].title}:{" "}
+            {productsInCart[parseInt(productId)]}
+          </div>
+        ))}
+      </div>
+      <div>
+        Total: ${" "}
+        {Object.keys(productsInCart).reduce(
+          (total, productId) =>
+            total +
+            productsObject[parseInt(productId)].price *
+              productsInCart[parseInt(productId)],
+          0
+        )}{" "}
+      </div>
     </div>
   );
 };
