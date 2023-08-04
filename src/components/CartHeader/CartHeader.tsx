8ee132ff8a4f7d6abaa6c7ab2d8d54b12761bac9
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-08-04 19:30:47 +0300

    added array id in cartheader

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -1,15 +1,22 @@
+import productsArray, { Product, getProductsObject } from "utils/productsArray";
+
 type Props = {
   productsInCart: {
     [id: number]: number;
   };
 };
-
+type ProductsObjectType = {
+  [id: number]: Product;
+};
 const CartHeader = ({ productsInCart }: Props) => {
+  const productsObject: ProductsObjectType = getProductsObject(productsArray);
+
   return (
     <div>
       {Object.keys(productsInCart).map((productId) => (
         <div key={productId}>
-          {productId}: {productsInCart[parseInt(productId)]}
+          {productsObject[parseInt(productId)].title}:{" "}
+          {productsInCart[parseInt(productId)]}
         </div>
       ))}
     </div>
--- src/conteiner/App/App.tsx
+++ src/conteiner/App/App.tsx
@@ -10,7 +10,6 @@ type ProductsInCartType = {
 
 const App = (props: Props) => {
   const [productsInCart, setProductsInCart] = useState<ProductsInCartType>({
-    // productsArray.map(ProductsInCartType.[id]) not work
     1: 3,
     2: 3,
   });
@@ -25,9 +24,9 @@ const App = (props: Props) => {
     <StyledEngineProvider injectFirst>
       <CssBaseline />
       <Header productsInCart={productsInCart} />
-      <button onClick={() => addProductToCart(2, 5)}>
+      {/* <button onClick={() => addProductToCart(2, 5)}>
         add to cart (id:2, count:5)
-      </button>
+      </button> */}
       <Main addProductToCart={addProductToCart} />
     </StyledEngineProvider>
   );
