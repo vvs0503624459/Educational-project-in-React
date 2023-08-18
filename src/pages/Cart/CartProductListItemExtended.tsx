9225e0e30e1f19bc0ec68775935929283b3ebf71
vvs0503624459 <106775038+vvs0503624459@users.noreply.github.com>
2023-08-18 23:22:43 +0300

    implemented toggle-like in Cart

--- src/pages/Cart/CartProductListItemExtended.tsx
+++ src/pages/Cart/CartProductListItemExtended.tsx
@@ -2,6 +2,9 @@ import { Button, Card, CardContent, Grid } from "@mui/material";
 import { Product } from "utils/productsArray";
 import DeleteIcon from "@mui/icons-material/Delete";
 import Quantity from "components/Quantity/Quantity";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { useAppDispatch, useAppSelector } from "redux/hooks";
 type Props = {
   product: Product;
   productCount: number;
@@ -17,10 +20,22 @@ const CartProductListItemExtended = ({
   changeProductQuantity,
   minCount,
 }: Props) => {
+  const isLiked = useAppSelector(
+    (state) => state.productsLikeState[product.id]
+  );
+  const dispatch = useAppDispatch();
   return (
     <Grid item xs={12} sm={4}>
       <Card variant="outlined">
         <CardContent>
+          <Button
+            variant="outlined"
+            onClick={() => {
+              dispatch({ type: "toggle_like", id: product.id });
+            }}
+          >
+            {isLiked ? <FavoriteIcon /> : <FavoriteBorderIcon />}
+          </Button>
           <div className="product-image">
             <img src={product.image} alt="" />
           </div>
